f90b8ca6f14b635ca32884a91c2acc71d14bec34
Areaxe <2860349275@qq.com>
2017-08-17 22:29:33 +0800

    add icon font

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -14,7 +14,8 @@ export default class Index extends Component{
 	}
 	render(){
 		return <div className="index-page">
-			<DatePicker />	
+			<DatePicker />
+			<div className="fa-calendar"></div>	
 		</div>
 	}
 }
